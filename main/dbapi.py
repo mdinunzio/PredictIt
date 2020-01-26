689d491773fa6912be569ec9f1654665cf69c33c
Michael DiNunzio <mdinunzio@gmail.com>
2020-01-26 16:51:58 -0500

    fixed ts option in get_tweet_counts

--- main/dbapi.py
+++ main/dbapi.py
@@ -11,7 +11,7 @@ from threading import Thread
 twitter_users = externapi.get_twitter_users()
 
 
-def store_tweet_counts(tweet_counts):
+def store_tweet_counts(tweet_counts, update_ts=None):
     engine = sqlalchemy.create_engine(
         'postgresql+psycopg2://{username:s}:{password:s}'
         '@{host:s}:{port:.0f}/{database:s}'
@@ -20,7 +20,9 @@ def store_tweet_counts(tweet_counts):
                 host='localhost',
                 port=5432,
                 database=authapi.pgdb.db_name))
-    tweet_counts['update_ts'] = datetime.datetime.now()
+    if update_ts is None:
+        update_ts = datetime.datetime.now()
+    tweet_counts['update_ts'] = update_ts
     tweet_counts.to_sql('tweetcounts', engine, if_exists='append',index=False)
 
 
