84dce60fb6c8ed5edb731d1c18df217acecc19a0
Michael DiNunzio <mdinunzio@gmail.com>
2020-02-01 11:27:54 -0500

    added tabs to print statements

--- main/main.py
+++ main/main.py
@@ -13,13 +13,13 @@ def run_main():
         try:
             update_ts = datetime.datetime.now()
             print(update_ts)
-            print('Pulling data')
+            print('\tPulling data')
             pi_data = predictit.get_pi_data()
             pi_df = predictit.get_pi_df(pi_data=pi_data)
             # TODO get prior DB users
             twitter_users = predictit.get_twitter_users(pi_data)
             tweet_counts = twitter.get_tweet_counts(twitter_users)
-            print('Storing data')
+            print('\tStoring data')
             predictit.store_pi_df(pi_df, update_ts)
             twitter.store_tweet_counts(tweet_counts, update_ts)
             print('\tSuccess')
