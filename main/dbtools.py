4ba59c1792dc14406022f07c6823365fc1e7494a
Michael DiNunzio <mdinunzio@gmail.com>
2020-02-08 15:36:52 -0500

    cleared db tools

--- main/dbtools.py
+++ main/dbtools.py
@@ -7,23 +7,3 @@ import sqlalchemy
 import time
 from threading import Thread
 
-
-twitter_users = predictit.get_twitter_users()
-
-
-def store_continuously():
-    while True:
-        try:
-            print(datetime.datetime.now())
-            print('\tStoring')
-            tweet_counts = twitter.get_tweet_counts(twitter_users)
-            store_tweet_counts(tweet_counts)
-            print('\tSuccess')
-        except Exception as e:
-            print(e)
-        time.sleep(60)
-
-
-def main():
-    t = Thread(target=store_continuously, daemon=True)
-    t.start()
