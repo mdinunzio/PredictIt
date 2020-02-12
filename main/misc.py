ec1c8f80c7de0f8856c947a18eca604431e011b5
Michael DiNunzio <mdinunzio@gmail.com>
2020-02-12 18:05:21 -0500

    moved meta to prod

--- main/misc.py
+++ main/misc.py
@@ -13,11 +13,11 @@ market_meta['update_ts'] = datetime.datetime.now()
 
 dbt.insert(market_meta, name='pimarketmeta', con=dbt.PI_DEV)
 
-q = "SELECT id_market FROM piapi GROUP BY id_market"
+q = "SELECT DISTINCT id_market FROM piapi"
 existing_market_ids = dbt.select(q, con=dbt.PI_PROD)
 existing_market_ids = existing_market_ids['id_market'].tolist()
 
-q = "SELECT market_id FROM pimarketmeta GROUP BY market_id"
+q = "SELECT DISTINCT market_id FROM pimarketmeta"
 meta_market_ids = dbt.select(q, con=dbt.PI_DEV)
 meta_market_ids = meta_market_ids['market_id'].tolist()
 
@@ -30,11 +30,13 @@ for i, mid in enumerate(grab_ids):
         market_meta = market_meta.to_frame().T
         market_meta['update_ts'] = datetime.datetime.now()
         dbt.insert(market_meta, name='pimarketmeta', con=dbt.PI_DEV)
-        time.sleep(1)
-        if i % 10 == 0:
+        time.sleep(2)
+        if i % 30 == 0:
             time.sleep(10)
     except Exception as e:
         print(e)
         break
 
+mm_dev = dbt.select('SELECT * FROM pimarketmeta', con=dbt.PI_DEV)
+dbt.insert(mm_dev, 'pimarketmeta', dbt.PI_PROD)
 
