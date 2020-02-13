68e790fa394fae1f09def56e95768f9e5a3b847f
Michael DiNunzio <mdinunzio@gmail.com>
2020-02-12 22:05:21 -0500

    fixed sql

--- sql/PiTwitterMeta.sql
+++ sql/PiTwitterMeta.sql
@@ -1,14 +1,14 @@
 CREATE TABLE public.pitweetmeta
 (
-	"market_id" integer NOT NULL REFERENCES pimarketmeta(market_id),
-	"update_ts" timestamp without time zone NOT NULL pimarketmeta(update_ts),
+	"market_id" integer NOT NULL,
+	"update_ts" timestamp without time zone NOT NULL,
 	"user" character varying(25),
 	"baseline" bigint,
 	"market_name" character varying(200),
 	"market_open_ts" timestamp without time zone,
-	"market_end_ts", timestamp without time zone,
+	"market_end_ts" timestamp without time zone,
 	"rule" text,
-    PRIMARY KEY ("market_id", "update_ts")
+    FOREIGN KEY ("market_id", "update_ts") REFERENCES pimarketmeta("market_id", "update_ts")
 );
 
 ALTER TABLE public.pitweetmeta
